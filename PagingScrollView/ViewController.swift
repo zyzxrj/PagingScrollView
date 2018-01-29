8729dba6a1983b118069185cc9e06578c8867fdf
limi <zhaoyan@hz-health.cn>
2018-01-29 17:06:54 +0800

    Initial Commit

---
+++ PagingScrollView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PagingScrollView
+//
+//  Created by mac on 29/1/18.
+//  Copyright © 2018年 mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
